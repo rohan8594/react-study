cf5602a7f1a29b424930fcbbfd52d2dea7a4f13b
rohan8594 <rohan.8594@gmail.com>
2019-01-11 19:22:04 +0530

    Added counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <span style={{ fontWeight: "bold" }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
